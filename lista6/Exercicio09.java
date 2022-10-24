deec5662b4c8924ed9ca5a67ba86f58b146505ac
nathanhermano <111927005+nathanhermano@users.noreply.github.com>
2022-10-24 19:28:06 -0300

    Add files via upload

---
+++ lista6/Exercicio09.java
@@ -0,0 +1,16 @@
+package lista6;
+
+import java.util.Scanner;
+
+public class Exercicio09 {
+	public static void main(String args[]) {
+		Scanner sc=new
+				Scanner(System.in);
+		System.out.println("Digite um número inteiro");
+		int numero=sc.nextInt();
+		if(numero % 2 == 0) {System.out.println("O número é par!");}
+		else {System.out.println("O número é ímpar");}
+		}
+	}
+
+
